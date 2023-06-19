008c6ed87c9fb39e5d095662a142cc05126116cf
Chansik Yun <chansigi@gmail.com>
2023-06-19 14:21:36 +0900

    .

--- add.cpp
+++ add.cpp
@@ -2,6 +2,12 @@
 #include "add.h"
 #include <iostream>
 
+void foo()
+{
+    std::cout << "foo" << std::endl;
+}
+
+// _add
 int add(int a, int b)
 {
     std::cout << "C++ add" << std::endl;
--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 
 #include "add.h"
 
+// __Z3foov
+void _Z3foov(void);
+
 int main(void)
 {
     int result = add(10, 20);
     printf("result: %d\n", result);
+
+    _Z3foov();
 
     return 0;
 }
