601dbe5a1d9ed92b1d7e865b35b3e84bca1818fb
Chansik Yun <chansigi@gmail.com>
2023-06-19 16:16:33 +0900

    .

--- 11_for.cpp
+++ 11_for.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+#include <string>
+
 int main()
 {
     int x[5] = { 1, 2, 3, 4, 5 };
@@ -13,5 +15,12 @@ int main()
     // C++11, Ranged-for
     for (auto e : x) {
         cout << e << endl;
+    }
+
+    // 순회 가능한 타입에 대해서 사용할 수 있습니다.
+    // - 배열, 문자열(string), 컨테이너
+    string s = "hello";
+    for (auto c : s) {
+        cout << c << endl;
     }
 }
