42fbff9e33bcaf4bc5932f5efee73ffa83a6042d
Chansik Yun <chansigi@gmail.com>
2023-06-20 11:46:52 +0900

    .

--- 16_stack4.cpp
+++ 16_stack4.cpp
@@ -1,20 +1,25 @@
-// 16_stack3.cpp
+// 16_stack4.cpp
 #include <iostream>
 using namespace std;
-
-// 멤버 데이터와 멤버 함수를 하나의 타입으로 만들 수 있습니다.
-//  => 캡슐화: 데이터(상태) + 함수(행위)
-//  => 상태와 행위를 가지는 구조체 변수 => 객체(상태 + 행위)
 
 // * 객체의 상태는 멤버 함수를 통해 조작되어야 합니다.
 //   "정보 은닉(Information Hiding)"
 //   => 접근 지정자
+//   - private: 외부에서 접근이 불가능합니다.
+//   -  public: 외부에서 접근이 가능합니다.
 
-struct Stack {
+// * struct
+//  => 기본 접근 지정자가 public 입니다.
+//   class
+//  => 기본 접근 지정자가 private 입니다.
+
+class Stack {
+private:
     // 멤버 데이터: 상태 / property
     int buff[10];
     int top;
 
+public:
     // 멤버 함수: 상태를 조작하는 행위 / method
     void init() { top = 0; }
 
@@ -34,7 +39,7 @@ int main()
     Stack s1;
     s1.init();
 
-    s1.top = -1; // !!!
+    // s1.top = -1; // !!!
 
     s1.push(10);
     s1.push(20);
