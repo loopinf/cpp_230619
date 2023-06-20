10def32f62f1e9e1a45535bd1820e2604782e381
Chansik Yun <chansigi@gmail.com>
2023-06-20 14:16:42 +0900

    .

--- 16_stack10.cpp
+++ 16_stack10.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 using namespace std;
 
-// C++ 표준라이브러리에 스택이 있습니다.
+// C++ 표준라이브러리(STL, Standard Template Library)에
+// 스택이 있습니다.
+// 1) 컨테이너(자료구조)
+//    vector: 동적 배열, 연속된 메모리
+//      list: 연결 리스트
+//     deque: vector 와 list의 절충형 자료구조
+//---------
+//     map / unordered_map: 사전 자료구조
+//     set / unordered_set: 집합 자료구조
+//---------
+//     stack / queue
+
+#if 0
 #include <stack>
 
 int main()
@@ -24,4 +36,26 @@ int main()
 
     cout << s.top() << endl;
     s.pop();
+}
+#endif
+
+#include <vector>
+
+int main()
+{
+    vector<int> v = { 10, 20, 30 };
+    cout << v.size() << endl;
+
+    v.push_back(100);
+    v.push_back(100);
+    v.push_back(100);
+    v.push_back(100);
+    cout << v.size() << endl;
+
+    cout << v[0] << endl;
+    cout << v[3] << endl;
+
+    for (auto e : v) {
+        cout << e << endl;
+    }
 }
