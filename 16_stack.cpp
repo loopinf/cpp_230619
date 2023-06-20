16657ea2891140c03e1a912af60dfe72f07f2323
Chansik Yun <chansigi@gmail.com>
2023-06-20 11:29:53 +0900

    .

---
+++ 16_stack.cpp
@@ -0,0 +1,29 @@
+// 16_stack.cpp
+#include <iostream>
+using namespace std;
+
+// LIFO(Last In First Out)
+
+int buff[10];
+int top = 0;
+
+void push(int n)
+{
+    buff[top++] = n;
+}
+
+int pop()
+{
+    return buff[--top];
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+
+    cout << pop() << endl;
+    cout << pop() << endl;
+    cout << pop() << endl;
+}
