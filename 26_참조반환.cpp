debc9d6bd3930c49ea21976ea44815094ea1a578
Chansik Yun <chansigi@gmail.com>
2023-06-21 15:33:10 +0900

    .

--- 26_참조반환.cpp
+++ 26_참조반환.cpp
@@ -77,6 +77,7 @@ int main()
 }
 #endif
 
+#if 0
 class User {
     string name;
     int age;
@@ -118,9 +119,138 @@ int main()
     user.SetAddress("Seoul");
     user.SetJob("Student");
 
+    // Builder Pattern
     // Chaining
     user.SetName("Tom")
         .SetAge(42)
         .SetAddress("Seoul")
         .SetJob("Student");
 }
+#endif
+
+// Telescoping Constructor Pattern => Java Idioms
+// => 생성자의 인자가 많을 경우, 빌더를 고려해라
+
+#if 0
+class User {
+    string name_; // 필수
+    int age_; // 옵션
+    string address_; // 옵션
+    string job_; // 옵션
+public:
+    User(const string& name)
+        : name_(name)
+    {
+    }
+
+    User(const string& name, int age)
+        : name_(name)
+        , age_(age)
+    {
+    }
+
+    User(const string& name, int age, const string& address)
+        : name_(name)
+        , age_(age)
+        , address_(address)
+    {
+    }
+
+    User(const string& name, int age, const string& address, const string& job)
+        : name_(name)
+        , age_(age)
+        , address_(address)
+        , job_(job)
+    {
+    }
+};
+
+int main()
+{
+    User user1("Tom");
+    User user2("Tom", 42);
+    User user3("Tom", 42, "Seoul");
+    User user4("Tom", 42, "Student", "Seoul");
+}
+#endif
+
+class User {
+    string name_; // 필수
+    int age_; // 옵션
+    string address_; // 옵션
+    string job_; // 옵션
+
+    User(const string& name, int age, const string& address, const string& job)
+        : name_(name)
+        , age_(age)
+        , address_(address)
+        , job_(job)
+    {
+    }
+
+    User(const UserBuilder& builder)
+        : name_(builder.name_)
+        , age_(builder.age_)
+        , address_(builder.address_)
+        , job_(builder.job_)
+    {
+    }
+
+    friend class UserBuilder;
+};
+
+class UserBuilder {
+    string name_; // 필수
+    int age_; // 옵션
+    string address_; // 옵션
+    string job_; // 옵션
+public:
+    UserBuilder(const string& name)
+        : name_(name)
+    {
+    }
+
+    UserBuilder& SetAge(int age)
+    {
+        age_ = age;
+        return *this;
+    }
+
+    UserBuilder& SetAddress(const string& address)
+    {
+        address_ = address;
+        return *this;
+    }
+
+    UserBuilder& SetJob(const string& job)
+    {
+        job_ = job;
+        return *this;
+    }
+
+    User Build()
+    {
+        return User(*this);
+    }
+
+    friend class User;
+};
+
+int main()
+{
+    User user = UserBuilder("Tom")
+                    .SetAge(42)
+                    .SetAddress("Seoul")
+                    .SetJob("Student")
+                    .Build();
+}
+
+#if 0
+int main()
+{
+    User user1("Tom");
+    User user2("Tom", 42);
+    User user3("Tom", 42, "Seoul");
+    User user4("Tom", 42, "Student", "Seoul");
+}
+#endif
