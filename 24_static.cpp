d44cedeb18a8a31bbf96886a4f0d9799ec5ab489
Chansik Yun <chansigi@gmail.com>
2023-06-21 13:40:47 +0900

    .

--- 24_static.cpp
+++ 24_static.cpp
@@ -6,11 +6,20 @@ using namespace std;
 // 2. 전역 변수는 정보 은닉이 불가능합니다.
 //  => 정적 멤버 변수
 
+// 3. 정적 멤버 변수를 접근하는 방법 2가지
+//  1) 클래스
+//     Car::cnt;
+
+//  2) 객체
+//     c1.cnt;
+
 class Car {
-    static int cnt; // 정적 멤버 변수 선언
+    // 정적 멤버 변수 선언
     int color;
 
 public:
+    static int cnt;
+
     Car()
     {
         ++cnt;
@@ -26,13 +35,16 @@ int Car::cnt = 0;
 
 int main()
 {
+    // 정적 멤버 데이터는 객체의 크기에 포함되지 않습니다.
     cout << sizeof(Car) << endl;
 
+    // 클래스를 통해서 접근 하는 방법
+    cout << Car::cnt << endl;
     Car c1, c2;
+    // 객체를 통해서 접근하는 방법
+    cout << c1.cnt << endl;
+
     Car cars[5];
-
-    // cout << c1.cnt << endl;
-
     cout << c1.GetCount() << endl;
     cout << c2.GetCount() << endl;
 }
