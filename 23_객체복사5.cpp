9b04fd4632a3f6d4f480a14ac2c50a499774a2ad
Chansik Yun <chansigi@gmail.com>
2023-06-22 09:19:13 +0900

    .

--- 23_객체복사5.cpp
+++ 23_객체복사5.cpp
@@ -5,6 +5,9 @@ using namespace std;
 // 복사 정책
 
 // 3) 복사 금지
+//   => 복사를 금지하면, 이동도 금지가 됩니다.
+//   => 소유권 이동은 같이 구현하는 경우가 많습니다.
+
 class User {
     char* name;
     int age;
@@ -12,11 +15,20 @@ class User {
     // 복사 금지 하는 방법
     // 1) private 영역에 선언만 합니다.
     // User(const User&);
+
     // 2) delete function => 복사 금지
     User(const User&) = delete;
     // User(User&&) = delete;
 
 public:
+    User(User&& rhs)
+        : name(rhs.name)
+        , age(rhs.age)
+    {
+        rhs.name = nullptr;
+        rhs.age = 0;
+    }
+
     User(const char* s, int n)
         : age(n)
     {
@@ -29,14 +41,26 @@ public:
         delete[] name;
     }
 
-    void Print() { cout << name << ", " << age << endl; }
+    void Print()
+    {
+        if (!name) {
+            printf("%p\n", name);
+            return;
+        }
+
+        cout << name << ", " << age << endl;
+    }
 };
+
+// 복사 금지 + 소유권 이동
+// => std::unique_ptr
 
 int main()
 {
     User user("Tom", 42);
     user.Print();
 
-    // User other = user;
-    // other.Print();
+    User other = std::move(user);
+    user.Print();
+    other.Print();
 }
