85ee4470652dd146e001163849384410eeca32df
Chansik Yun <chansigi@gmail.com>
2023-06-22 09:36:09 +0900

    .

--- 23_객체복사6.cpp
+++ 23_객체복사6.cpp
@@ -84,12 +84,14 @@ void Swap(T& a, T& b)
 //    컴파일러가 멤버를 이동하는 이동 생성자를 제공합니다.
 
 // 3. 복사 생성자를 금지하면, 이동 생성자도 금지됩니다.
-
+#if 0
 class AAA {
 public:
     AAA() { }
     // AAA(const AAA& rhs) = delete;
 };
+
+// std::move => lvalue를 rvalue로 변환하는 기능을 제공합니다.
 
 int main()
 {
@@ -100,4 +102,47 @@ int main()
 
     cout << a << endl;
     cout << b << endl;
+}
+#endif
+
+class Sample {
+    char* name;
+
+public:
+    Sample(const char* s)
+    {
+        name = new char[strlen(s) + 1];
+        strcpy(name, s);
+    }
+
+    ~Sample()
+    {
+        delete[] name;
+    }
+
+    Sample(const Sample& rhs)
+    {
+        name = new char[strlen(rhs.name) + 1];
+        strcpy(name, rhs.name);
+    }
+
+    Sample(Sample&& rhs)
+        : name(rhs.name)
+    {
+        rhs.name = nullptr; // !!!
+    }
+};
+
+// unique_ptr: 자원의 독점 - 복사 금지 + 소유권 이동
+// shared_ptr: 자원의 공유 - 참조 계수
+
+int main()
+{
+    Sample s("Tom");
+
+    Sample s1 = s; // 복사 생성자 호출
+    Sample s2 = std::move(s); // 이동 생성자 호출
+    // s는 자원을 더이상 참조하고 있지 않습니다.
+
+    Sample s3 = Sample("Bob"); // 이동 생성자 호출
 }
