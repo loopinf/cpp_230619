7bc993ec14ab9a42c99e0b2990485e1bf6185b84
Chansik Yun <chansigi@gmail.com>
2023-06-23 15:02:32 +0900

    .

--- 33_STL6.cpp
+++ 33_STL6.cpp
@@ -1,7 +1,9 @@
 // 33_STL6.cpp
 #include <iostream>
+#include <string>
 using namespace std;
 
+#if 0
 class Sample {
     int data = 100;
 
@@ -28,4 +30,59 @@ int main()
 {
     Sample s;
     s.go();
+}
+#endif
+
+#include <functional>
+// : function
+// => 범용 함수 포인터
+
+class Button {
+    string name;
+
+    function<void()> handler;
+
+public:
+    Button(const string& s)
+        : name { s }
+    {
+    }
+
+    void SetHandler(function<void()> h)
+    {
+        handler = h;
+    }
+
+    void Click()
+    {
+        handler();
+    }
+};
+
+void foo() { cout << "foo" << endl; }
+
+class Window {
+public:
+    // void Close(Window* this)
+    void Close()
+    {
+        cout << "Window close" << endl;
+    }
+};
+
+int main()
+{
+    Button button("닫기");
+    Window window;
+
+    button.SetHandler(foo);
+    button.SetHandler([] {
+        cout << "lambda" << endl;
+    });
+
+    button.SetHandler(bind(&Window::Close, window));
+
+    // bind(&Window::Close, window);
+
+    button.Click();
 }
