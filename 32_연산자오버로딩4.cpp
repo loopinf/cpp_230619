79707dac65c0025a83b0e3f24b6085fe868f0dad
Chansik Yun <chansigi@gmail.com>
2023-06-23 11:13:41 +0900

    .

---
+++ 32_연산자오버로딩4.cpp
@@ -0,0 +1,47 @@
+// 32_연산자오버로딩4.cpp
+#include <iostream>
+using namespace std;
+
+class Point {
+    int x;
+    int y;
+
+public:
+    Point(int a, int b)
+        : x(a)
+        , y(b)
+    {
+    }
+
+    int GetX() const { return x; }
+    int GetY() const { return y; }
+
+    void SetX(int a) { x = a; }
+    void SetY(int b) { y = b; }
+};
+
+ostream& operator<<(ostream& os, const Point& pt)
+{
+    return os << pt.GetX() << ", " << pt.GetY();
+}
+
+istream& operator>>(istream& is, Point& pt)
+{
+    int x, y;
+    is >> x >> y;
+    pt.SetX(x);
+    pt.SetY(y);
+
+    return is;
+}
+
+int main()
+{
+    Point pt(10, 20);
+
+    cin >> pt;
+    // operator>>(cin, pt)
+
+    cout << pt << endl;
+    // operator<<(cout, pt)
+}
